1625ae074ea05b917f7bb4914e62caacb69df4c8
gonzo-yokohama <73865585+gonzo-yokohama@users.noreply.github.com>
2021-01-25 19:25:17 +0900

    react.jsのサンプル

---
+++ src_sample/src_first/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const name = 'cocoto taro';
+const element = <h1>Hello, {name}</h1>;
+
+ReactDOM.render(
+	  element,
+	  document.getElementById('root')
+);
